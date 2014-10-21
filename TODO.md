c59ddc1979478daa344abec6d383456add581dec
Sebastien Guimont <sebastieng@sympatico.ca>
2014-10-21 08:29:58 -0400

    Initial commit

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO
+
+* Try to remove code duplications
+* Test
+* Documentation
+* Procedure to register this node module in npm repository
---
+++ test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').use(require('chai-as-promised')).expect;
+
+describe('Signer', function () {
+  describe('create a signer', function () {
+    it('should throw error if a property is missing', function () {
+      // TODO
+    });
+  });
+});
